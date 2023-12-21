4238ec0aad64578759ff3a10971c343aa252c69f
Kavin Phabiani <kavinphab@gmail.com>
2023-12-21 15:28:03 -0800

    initial commit

---
+++ src/Components/landing.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Picofme from './picofme.jpg';
+
+const Landing = () => {
+    return (
+        <div className='w-full h-[100vh] flex justify-center items-center'>
+            
+            <div>
+                <h1 className='text-5xl'>Hi, I'm Kavin. Welcome to my website!</h1>
+                <br/>
+                <p className='flex justify-center'>I am a student and aspiring developer.</p>
+            </div>
+        </div>
+        
+    );
+};
+
+export default Landing;
